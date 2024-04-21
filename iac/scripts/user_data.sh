11f8ae9955c384cd1ab6e2b6085d39cae8c9cdb8
chiplonton <chipkogi010790@gmail.com>
2024-04-21 16:57:37 +0700

    add scripts

---
+++ iac/scripts/user_data.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+sudo amazon-linux-extras install docker
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo usermod -a -G docker ec2-user
+sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
